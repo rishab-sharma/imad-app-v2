968e9f4da44cbe819dc79f44e56ffcd2dc41ec29
Rishab Sharma <rishusharma.sharma7@gmail.com>
2017-03-20 10:44:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,20 @@
-console.log('Loaded!');
+//console.log('Loaded!');
+var button = document.getElementById("counter");
+
+button.onclick = function() {
+    
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function() {
+    if (request.readystate === XMLHttpRequest.DONE){
+        if (request.status === 200){
+            var counter = request.responsetext;
+            var span = document.getElementById("count");
+            span.innerHTML = counter.toString();
+        }
+    }
+};  
+request.open('GET','http://rishab-sharma.imad.hasura-app.io/counter',true);
+request.send(null);
+    
+};
