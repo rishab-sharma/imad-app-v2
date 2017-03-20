b70be60c45db7b676aa01679767fe8f3ccf0a037
Rishab Sharma <rishusharma.sharma7@gmail.com>
2017-03-20 11:06:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var counter = 0;
 var app = express();
 app.use(morgan('combined'));
 
@@ -13,6 +12,7 @@ app.get('/about_me',function(req,res){res.sendFile(path.join(__dirname,'ui','abo
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+var counter = 0;
 app.get('/counter', function (req, res) {
     counter = counter + 1;
   res.send(counter.toString());
