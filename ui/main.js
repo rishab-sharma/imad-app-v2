b057d1adece4c27c183b9576d97da139549e950d
Rishab Sharma <rishusharma.sharma7@gmail.com>
2017-03-20 11:10:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ request.onreadystatechange = function() {
     if (request.readystate === XMLHttpRequest.DONE){
         if (request.status === 200){
             var counter = request.responseText;
-            var span = document.getElementById("count");
+            var span = document.getElementById('count');
             span.innerHTML = counter.toString();
         }
     }
