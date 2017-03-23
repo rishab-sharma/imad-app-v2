468709920e87e3cb183ab5bc63898fed5b92e3f8
Rishab Sharma <rishusharma.sharma7@gmail.com>
2017-03-23 20:54:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,15 +17,28 @@ request.onreadystatechange = function() {
 request.open('GET','http://rishab-sharma.imad.hasura-app.io/counter',true);
 request.send(null);         
 };
+
 var nameInput =document.getElementById("name");
 var name = nameInput.value;
 var submit = document.getElementById('submit1');
 submit.onclick = function() {
-var names = ['name1','nmae2','name3'];
-var list='';
-for (var i=0;i<names.length;i++){
-    list += '<li>' + names[i] + '</li>';
-}
-var ul = document.getElementById('namelist');
-ul.innerHTML = list;
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function() {
+    if (request.readyState === XMLHttpRequest.DONE){
+        if (request.status === 200){
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list='';
+            for (var i=0;i<names.length;i++){
+               list += '<li>' + names[i] + '</li>';
+            }
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list;
+        }
+    }
+};  
+request.open('GET','http://rishab-sharma.imad.hasura-app.io/submit-name/'+name,true);
+request.send(null);         
+
 };
