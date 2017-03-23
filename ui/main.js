8f052be7c98c591f7efda7b4fb632de36be2f72c
Rishab Sharma <rishusharma.sharma7@gmail.com>
2017-03-23 20:21:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,7 @@ var submit = document.getElementById('submit1');
 submit.onclick = function() {
 var names = ['name1','nmae2','name3'];
 var list='';
-for (var i=0;i<nmaes.length;i++){
+for (var i=0;i<names.length;i++){
     list += '<li>' + names[i] + '</li>';
 }
 var ul = document.getElementById('namelist');
