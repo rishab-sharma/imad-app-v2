e8991e680026aadd480bfd4d8cede2ddaa79b91b
Rishab Sharma <rishusharma.sharma7@gmail.com>
2017-06-17 06:04:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/about_me',function(req,res){res.sendFile(path.join(__dirname,'ui','about_me.html'));
+app.get('/draw',function(req,res){res.sendFile(path.join(__dirname,'ui','draw.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
