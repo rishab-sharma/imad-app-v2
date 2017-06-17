fa71e9dd50c5318bd7a9f14b0bbf7acd8a6d7f79
Rishab Sharma <rishusharma.sharma7@gmail.com>
2017-06-17 06:17:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,14 +23,14 @@ app.get('/main.js', function (req, res) {
 app.get('/package-json.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'package-json.js'));
 });
-app.get('/ui/school-logo.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'school-logo.jpg'));
+app.get('/bootstrap.min.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'bootstrap.min.js'));
 });  
-app.get('/ui/school-logo2.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'school-logo2.jpg'));
+app.get('/bootstrap.min.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'bootstrap.min.css'));
 });  
-app.get('/ui/slider.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'slider.jpg'));
+app.get('/responsive.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'responsive.css'));
 });
 var names=[];
 app.get('/submit-name/:name', function (req, res) {
@@ -39,9 +39,7 @@ app.get('/submit-name/:name', function (req, res) {
   //JSON javascript object notation
   res.send(JSON.stringify(names));
 });
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
