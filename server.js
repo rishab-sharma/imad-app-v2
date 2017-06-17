4395a279136d1a48a789fd85838161f3348f7541
Rishab Sharma <rishusharma.sharma7@gmail.com>
2017-06-17 06:09:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.get('/counter', function (req, res) {
     counter = counter + 1;
   res.send(counter.toString());
 });
-app.get('/ui/collg-logo.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'collg-logo.png'));
+app.get('/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 app.get('/ui/rs.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'rs.jpg'));
